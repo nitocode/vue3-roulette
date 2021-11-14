e01c8975002f01145c99eb8cc010396b4944712d
nitocode <lu.nitoumbi@gmail.com>
2021-11-14 10:00:57 +0100

    Update: base url

--- pages/router.js
+++ pages/router.js
@@ -3,10 +3,10 @@ import Home from "./views/Home.vue"
 import Docs from "./views/Docs.vue"
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory("/vue3-roulette/"),
     routes: [
-      { path: '/', component: Home },
-      { path: '/docs', component: Docs },
+      { path: "/", component: Home },
+      { path: "/docs", component: Docs },
     ],
   })
   
