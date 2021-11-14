c8e14bb2cf2740b48b641099d49818c355912528
nitocode <lu.nitoumbi@gmail.com>
2021-11-14 10:32:40 +0100

    Remove: base url

--- pages/router.js
+++ pages/router.js
@@ -3,10 +3,10 @@ import Home from "./views/Home.vue"
 import Docs from "./views/Docs.vue"
 
 export const router = createRouter({
-    history: createWebHistory("/vue3-roulette/"),
+    history: createWebHistory(),
     routes: [
-      { path: "/", component: Home },
-      { path: "/docs", component: Docs },
+      { path: '/', component: Home },
+      { path: '/docs', component: Docs },
     ],
   })
   
