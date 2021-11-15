3e518465fa4f2c03274b2ee9e489104b10c722a4
nitocode <lu.nitoumbi@gmail.com>
2021-11-15 03:03:08 +0100

    Add: github pages workaround on vuejs app

--- pages/App.vue
+++ pages/App.vue
@@ -13,6 +13,14 @@ import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 export default {
   name: "Container",
-  components: { Navbar, Footer }
+  components: { Navbar, Footer },
+  mounted() {
+    // Github pages SPA workaround
+    let path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.$router.push(path);
+    }
+  }
 }
 </script>
